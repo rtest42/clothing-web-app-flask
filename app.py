a8ebc2f3c5fe53121868ffac6faa2cc09dd1e055
Royce Li <royce95129@gmail.com>
2024-02-29 21:22:30 -0800

    Connected search to backend and frontend

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import sys
+import requests
+import os
+from operator import itemgetter
+from decimal import Decimal
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
@@ -6,9 +12,53 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/search')
+@app.route('/search_fail')
+def search_fail():
+    return render_template('search_fail.html')
+
+@app.route('/search', methods=['GET', 'POST'])
 def search():
-    return render_template('search.html')
+    if request.method == 'POST':
+        # SerpAPI key obtained by signing up
+        # Default plan: 100 uses per month
+        SERPAPI_KEY = os.getenv('SERPAPI_KEY')
+
+        search_query = request.form.get('search')
+        wearable_keywords = []
+        with open('wearable_keywords.txt', 'r') as file:
+            for keyword in file:
+                wearable_keywords.append(keyword.replace('\n', ''))
+
+        # Do not perform search if not clothing-related
+        is_wearable = False
+        search_query = search_query.lower().strip()
+        for wearable in wearable_keywords:
+            if search_query.find(wearable) >= 0:
+                is_wearable = True
+                break
+
+        if not is_wearable:
+            return render_template('search_fail.html')
+
+        # Search using SerpAPI
+        url = 'https://serpapi.com/search'
+        params = {
+            'q': search_query,
+            'api_key': SERPAPI_KEY,
+            'engine': 'google_shopping'
+        }
+
+        # Get results in JSON
+        response = requests.get(url, params=params)
+        results = response.json()
+
+        # Sort
+        shopping_results = results['shopping_results']
+        shopping_results = sorted(shopping_results, key=lambda k: Decimal(k['price'].strip('$')))
+
+        return render_template('search_passed.html', results=shopping_results)
+    else:
+        return render_template('search.html')
 
 @app.route('/shopping-cart')
 def shopping_cart():
@@ -19,4 +69,6 @@ def user_photo_upload():
     return render_template('user_photo_upload.html')
 
 if __name__ == '__main__':
+    load_dotenv()
     app.run(debug=True)
+    
--- search.py
+++ search.py
@@ -5,47 +5,51 @@ from operator import itemgetter
 from decimal import Decimal
 from dotenv import load_dotenv
 
-load_dotenv()
+def main():
+    load_dotenv()
 
-# SerpAPI key obtained by signing up
-# Default plan: 100 uses per month
-SERPAPI_KEY = os.getenv('SERPAPI_KEY')
+    # SerpAPI key obtained by signing up
+    # Default plan: 100 uses per month
+    SERPAPI_KEY = os.getenv('SERPAPI_KEY')
 
-search_query = input('Search for: ')
+    search_query = input('search: ')
 
-wearable_keywords = []
-with open('wearable_keywords.txt', 'r') as file:
-    for keyword in file:
-        wearable_keywords.append(keyword.replace('\n', ''))
+    wearable_keywords = []
+    with open('wearable_keywords.txt', 'r') as file:
+        for keyword in file:
+            wearable_keywords.append(keyword.replace('\n', ''))
 
-# Do not perform search if not clothing-related
-is_wearable = False
-search_query = search_query.lower()
-for wearable in wearable_keywords:
-    if search_query.find(wearable) > 0:
-        is_wearable = True
-        break
+    # Do not perform search if not clothing-related
+    is_wearable = False
+    search_query = search_query.lower()
+    for wearable in wearable_keywords:
+        if search_query.find(wearable) >= 0:
+            is_wearable = True
+            break
 
-if not is_wearable:
-    print('Not a valid search term!')
-    sys.exit(1)
+    if not is_wearable:
+        print('Not a valid search term!', file=sys.stderr)
+        sys.exit(1)
 
-# Search using SerpAPI
-url = 'https://serpapi.com/search'
-params = {
-    'q': search_query,
-    'api_key': SERPAPI_KEY,
-    'engine': 'google_shopping'
-}
+    # Search using SerpAPI
+    url = 'https://serpapi.com/search'
+    params = {
+        'q': search_query,
+        'api_key': SERPAPI_KEY,
+        'engine': 'google_shopping'
+    }
 
-# Get results in JSON
-response = requests.get(url, params=params)
-results = response.json()
+    # Get results in JSON
+    response = requests.get(url, params=params)
+    results = response.json()
 
-# Sort
-shopping_results = results['shopping_results']
-shopping_results = sorted(shopping_results, key=lambda k: Decimal(k['price'].strip('$')))
+    # Sort
+    shopping_results = results['shopping_results']
+    shopping_results = sorted(shopping_results, key=lambda k: Decimal(k['price'].strip('$')))
 
-# Format output
-for result in shopping_results:
-    print(result['title'] + ' - ' + result['price'] + '\n' + result['link'] + '\n')
+    # Format output
+    for result in shopping_results:
+        print(result['title'] + ' - ' + result['price'] + '\n' + result['link'] + '\n')
+
+if __name__ == '__main__':
+    main()
