beb1a7ae5a113cf27e9c2dd77b16a3905e7cb6da
Royce Li <royce95129@gmail.com>
2024-03-22 00:25:08 -0700

    Added login system

--- app.py
+++ app.py
@@ -8,11 +8,12 @@ from dotenv import load_dotenv
 app = Flask(__name__)
 database = "armoire.db"
 results = None
+user = ""
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', user=user)
 
 
 @app.route('/search_fail')
@@ -56,7 +57,7 @@ def search():
         # Get results in JSON
         response = requests.get(url, params=params)
         shopping_results = response.json()['shopping_results']
-        return render_template('search_passed.html', results=shopping_results)
+        return render_template('search_passed.html', results=shopping_results, user=user)
     else:
         return render_template('search.html')
 
@@ -69,7 +70,7 @@ def user_photo_upload():
 @app.route('/shop/', methods=['GET', 'POST'])
 def shop():
     if request.method == 'GET':
-        return render_template('shop.html', shopping_err=False)
+        return render_template('shop.html', shopping_err=False, user=user)
     else:
         global results
         if request.form.get('submit') == 'Submit':
@@ -92,7 +93,7 @@ def shop():
 
             # Do not perform search if not clothing-related
             if not is_wearable:
-                return render_template('shop.html', shopping_err=True)
+                return render_template('shop.html', shopping_err=True, user=user)
 
             dropdown = {
                 'relevance': 'p_ord:r',
@@ -121,11 +122,11 @@ def shop():
                 title = result['title']
                 price = result['extracted_price']
                 link = result['link']
-                sql_cmd = "INSERT INTO armoire (title, price, link) VALUES (?, ?, ?)"
-                cursor.execute(sql_cmd, (title, price, link))
+                sql_cmd = "INSERT INTO cart (username, title, price, link) VALUES (?, ?, ?, ?);"
+                cursor.execute(sql_cmd, (user, title, price, link))
                 db.commit()
 
-        return render_template('shop.html', shopping_err=False, results=results)
+        return render_template('shop.html', shopping_err=False, results=results, user=user)
 
 
 @app.route('/cart', methods=['GET', 'POST'])
@@ -133,37 +134,92 @@ def cart():
     if request.method == 'GET':
         with sqlite3.connect(database) as db:
             cursor = db.cursor()
-            sql_cmd = "SELECT * FROM armoire"
-            cursor.execute(sql_cmd)
+            sql_cmd = "SELECT * FROM cart WHERE username = ?"
+            cursor.execute(sql_cmd, (user,))
             shopping_cart = cursor.fetchall()
             db.commit()
 
-        return render_template('cart.html', cart=shopping_cart)
+        return render_template('cart.html', cart=shopping_cart, user=user)
     else:
         with sqlite3.connect(database) as db:
             cursor = db.cursor()
             item_id = request.form.get('submit')
-            sql_cmd = "DELETE FROM armoire WHERE id = ?"
+            sql_cmd = "DELETE FROM cart WHERE id = ?"
             cursor.execute(sql_cmd, (item_id,))
-            db.commit()
-            sql_cmd = "SELECT * FROM armoire"
+            sql_cmd = "SELECT * FROM cart"
             cursor.execute(sql_cmd)
             shopping_cart = cursor.fetchall()
             db.commit()
 
-        return render_template('cart.html', cart=shopping_cart)
+        return render_template('cart.html', cart=shopping_cart, user=user)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    global user
+    if request.method == 'GET':
+        return render_template('login.html', err_login=False, user=user)
+    else:
+        with sqlite3.connect(database) as db:
+            cursor = db.cursor()
+            sql_cmd = "SELECT * FROM users WHERE username = ?"
+            cursor.execute(sql_cmd, (request.form.get("username"),))
+            get_user = cursor.fetchall()
+            db.commit()
+            if request.form.get("username") == '' or len(get_user) != 1 or get_user[0][2] != request.form.get("password"):
+                return render_template('login.html', err_login=True, user=user)
+
+            user = request.form.get("username")
+            return render_template('index.html', user=user)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html', err_register=False, user=user)
+    else:
+        with sqlite3.connect(database) as db:
+            cursor = db.cursor()
+            sql_cmd = "SELECT * FROM users WHERE username = ?"
+            cursor.execute(sql_cmd, (request.form.get("username"),))
+            get_user = cursor.fetchall()
+            if len(get_user) != 0:
+                return render_template('register.html', err_register=True, user=user)
+
+            sql_cmd = "INSERT INTO users (username, password) VALUES (?, ?);"
+            cursor.execute(sql_cmd, (request.form.get("username"), request.form.get("password")))
+            db.commit()
+            return render_template('index.html', user=user)
+
+
+@app.route('/logout')
+def logout():
+    global user
+    user = ""
+    return render_template('index.html', user=user)
 
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    return render_template('contact.html', user=user)
 
 
 def init_database():
+    if not os.path.isfile(database):
+        open(database, 'x').close()
+
     with sqlite3.connect(database) as db:
         cursor = db.cursor()
-        sql_cmd = ("CREATE TABLE IF NOT EXISTS armoire (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, price REAL, "
-                   "link TEXT);")
+        sql_cmd = ("CREATE TABLE IF NOT EXISTS cart (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, "
+                   "title TEXT, price REAL, link TEXT);")
+        cursor.execute(sql_cmd)
+        sql_cmd = ("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT NOT "
+                   "NULL, password TEXT NOT NULL);")
+        cursor.execute(sql_cmd)
+        sql_cmd = "CREATE UNIQUE INDEX IF NOT EXISTS username ON users (username);"
+        cursor.execute(sql_cmd)
+        # Guest user
+        sql_cmd = "INSERT OR IGNORE INTO users (username, password) VALUES ('', '');"
         cursor.execute(sql_cmd)
         db.commit()
 
